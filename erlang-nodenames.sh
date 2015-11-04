3aeb2b76690fb9b994ff934a3fe622f4cf3c0901
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2015-11-04 09:34:54 +0100

    erlang-nodenames.sh: Script to see what hostname Erlang picks
    The script is executed on Travis CI.

---
+++ erlang-nodenames.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# vim:sw=4:et:
+
+set -x
+
+hostname
+
+hostname -s
+
+hostname -f
+
+erl -A0 -noinput -boot start_clean -sname rabbit -eval \
+    'io:format("~s~n", [node()]),
+     halt().'
+
+erl -A0 -noinput -boot start_clean -name rabbit -eval \
+    'io:format("~s~n", [node()]),
+     halt().'
+
+erl -A0 -noinput -boot start_clean -eval \
+    'net_kernel:start([rabbit, shortnames]),
+     io:format("~s~n", [node()]),
+     halt().'
+
+erl -A0 -noinput -boot start_clean -eval \
+    'net_kernel:start([rabbit, longnames]),
+     io:format("~s~n", [node()]),
+     halt().'
