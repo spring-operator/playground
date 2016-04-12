335d41f7aeee4bd2037aa0769440ab8c4870b749
Brandon Shroyer <bshroyer@pivotal.io>
2016-04-12 16:12:38 -0400

    Target output repo in CI

--- ci/test.sh
+++ ci/test.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone playground updated-playground
 
-pushd playground
+pushd updated-playground
   echo "quack" >> commentary.txt
   git config --global user.email "dummy@rabbitmq.com"
   git config --global user.name "RabbitMQ CI"
