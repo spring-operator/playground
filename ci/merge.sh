083ff1ccc157dd23affb13d6d37fead4531482fa
Brandon Shroyer <bshroyer@pivotal.io>
2016-04-12 16:21:00 -0400

    Convert one-step commit to two-step merge

--- ci/merge.sh
+++ ci/merge.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-git clone playground updated-playground
-
 pushd updated-playground
-  git co -b test-concourse
-  echo "quack" >> commentary.txt
   git config --global user.email "dummy@rabbitmq.com"
   git config --global user.name "RabbitMQ CI"
-  git commit -am "Added insightful commentary"
+  git co stable
+  git merge test-concourse
+  git branch -d test-concourse
 popd
