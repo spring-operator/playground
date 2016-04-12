6136fad8457f5957df66ec0c4886e200026ccbe0
Brandon Shroyer <bshroyer@pivotal.io>
2016-04-12 15:48:53 -0400

    Add dummy github configs to CI

--- ci/test.sh
+++ ci/test.sh
@@ -4,6 +4,8 @@ set -ex
 
 pushd playground
   echo "quack" >> commentary.txt
+  git config --global user.email ""
+  git config --global user.name "RabbitMQ CI"
   git add .
-  git commit -m "Added insightful commentary"
+  git commit -am "Added insightful commentary"
 popd
