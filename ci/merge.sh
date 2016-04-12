c20b823bcbd30688797398135b34ad0924b0fb9a
Brandon Shroyer <bshroyer@pivotal.io>
2016-04-12 16:40:28 -0400

    Removing another git command alias

--- ci/merge.sh
+++ ci/merge.sh
@@ -7,7 +7,7 @@ git clone updated-playground merged-playground
 pushd merged-playground
   git config --global user.email "dummy@rabbitmq.com"
   git config --global user.name "RabbitMQ CI"
-  git co stable
+  git checkout stable
   git merge test-concourse
   git branch -d test-concourse
 popd
