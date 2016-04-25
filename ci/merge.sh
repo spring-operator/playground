eea0664a7aec1d88936e749ce73a394d292eec7c
Brandon Shroyer <bshroyer@pivotal.io>
2016-04-25 15:28:01 -0400

    Remove commit script and modify merge pipeline

--- ci/merge.sh
+++ ci/merge.sh
@@ -2,13 +2,12 @@
 
 set -ex
 
-git clone updated-playground merged-playground
+git clone playground merged-playground
 
 (
   cd merged-playground
   git config --global user.email "dummy@rabbitmq.com"
   git config --global user.name "RabbitMQ CI"
   git checkout stable
-  git merge test-concourse
-  git branch -d test-concourse
+  git merge develop
 )
